82032c593db89036dff6b2587e52da1726217963
Michael Sumner <mdsumner@gmail.com>
2016-04-08 14:50:34 +1000

    source note

--- R/bgmfiles-package.r
+++ R/bgmfiles-package.r
@@ -1,5 +1,8 @@
 #' bgmfiles.
 #'
+#' Raw data files are maintained here: https://github.com/AustralianAntarcticDivision/box-geometry-models.git
+#'
+#' This package contains a simple workflow to update from that source repo and install the raw files into extdata/ of the installed package.
 #' @name bgmfiles
 #' @docType package
 NULL
